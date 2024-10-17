8a3e5ff1aefb06a6526f37fc4c129f86fc674af2
Evan Perry <eperry2688@gmail.com>
2024-10-17 17:32:54 -0600

    13.7 Middleware for put and post

--- exercises/controllers/blogs.js
+++ exercises/controllers/blogs.js
@@ -2,17 +2,29 @@ const router = require('express').Router()
 
 const { Blog } = require('../models')
 
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  // Handle specific errors
+  if (error.name === 'SequelizeValidationError') {
+    return res.status(400).json({ error: 'Validation error' });
+  }
+
+  // Other potential errors
+  return res.status(500).json({ error: 'An unexpected error occurred' });
+};
+
 router.get('/', async (req, res) => {
   const blogs = await Blog.findAll()
   res.json(blogs)
 })
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
   try {
     const blog = await Blog.create(req.body)
     res.json(blog)
   } catch(error) {
-    return res.status(400).json({ error })
+    next(error)
   }
 })
 
@@ -36,14 +48,16 @@ router.delete('/:id', blogFinder, async (req, res) => {
   res.status(204).end()
 })
 
-router.put('/:id', blogFinder, async (req, res) => {
-  if (req.blog) {
+router.put('/:id', blogFinder, async (req, res, next) => {
+  try {
     req.blog.likes = req.body.likes
     await req.blog.save()
     res.json(req.blog.likes)
-  } else {
-    res.status(404).end()
+  } catch (error) {
+    next(error)
   }
 })
+
+router.use(errorHandler)
 
 module.exports = router
